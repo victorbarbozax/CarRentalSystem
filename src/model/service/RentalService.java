3c232c5d7a01cc35618bb40e3967567e59034625
Victor Barboza <victor.barboza052008@gmail.com>
2024-10-25 16:44:38 -0300

    Interface Implementation

--- src/model/service/RentalService.java
+++ src/model/service/RentalService.java
@@ -10,9 +10,10 @@ public class RentalService {
 	private Double pricePerHour;
 	private Double pricePerDay;
 	
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
-	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
+		super();
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
 		this.taxService = taxService;
